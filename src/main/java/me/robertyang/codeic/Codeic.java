59a056d1c709826109630334e7f751103e7540fc
Robert <RobertIndie@outlook.com>
2017-02-19 20:34:44 +0800

    Initialize proxy.

--- src/main/java/me/robertyang/codeic/Codeic.java
+++ src/main/java/me/robertyang/codeic/Codeic.java
@@ -1,5 +1,48 @@
 package me.robertyang.codeic;
 
-public class Codeic {
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import org.apache.logging.log4j.Logger;
+
+import me.robertyang.common.CommonProxy;
+
+/**
+ * Codeic main class.
+ */
+@Mod(modid = Codeic.modid,version = Codeic.version)
+public class Codeic {
+	
+	//MOD basic elements
+	public static final String modid = "codeic";
+	public static final String version = "1.0.0";
+	public static Logger logger;
+    @SidedProxy(clientSide = "com.github.ustc_zzzz.fmltutor.client.ClientProxy", serverSide = "com.github.ustc_zzzz.fmltutor.common.CommonProxy")
+    public static CommonProxy proxy;
+		
+    @Instance(modid)
+    public static Codeic instance;
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        proxy.postInit(event);
+    }
 }
