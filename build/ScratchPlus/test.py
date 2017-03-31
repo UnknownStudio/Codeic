22850e1f2d0f1c076d8b30872615364965409058
Robert Yang <RobertIndie@outlook.com>
2017-03-31 22:45:51 +0800

    Update

--- build/ScratchPlus/test.py
+++ build/ScratchPlus/test.py
@@ -6,20 +6,35 @@ label_whenReceive = "when I receive %s"
 
 Codeic_setBackgroundColor = "Codeic_setBackgroundColor"
 Codeic_regeisterEvent = "Codeic_regeisterEvent"
+Codeic_regeisterSprite = "Codeic_regeisterSprite"
 
+#const value
+build_path = ".\\build"
 #global value
 eventID = 0
+valueID = 0
+spriteID = 0
+globalValueDict = {};
 
 def tostring(o):
 	if isinstance(o,int):
 		return tostring(str(o))
 	return o.encode('utf-8')
-	
+
+def getVariableID(name):
+	return tostring(globalValueDict[name])
+
 def setVariable(name,value):
-	return "CodeicVar_"+tostring(name)+"=\""+tostring(value)+"\"\n"
+	global valueID
+	globalValueDict[name] = valueID
+	valueID = valueID + 1
+	return 'CodeicVar_{}="{}"--{}\n'.format(getVariableID(name),tostring(value),name)
 	
 def setList(name,list):
-	result = "CodeicVar_"+name + "={"
+	global valueID
+	globalValueDict[name] = valueID
+	valueID = valueID +1
+	result = "CodeicVar_"+getVariableID(name) + "={"
 	for item in list.items:
 		result += "\"" + tostring(item) + "\","
 	
@@ -29,8 +44,8 @@ def setList(name,list):
 	
 def setEvent(script,message):
 	global eventID
-	result = Codeic_regeisterEvent + "(" + tostring(eventID) + ",\"" + message + "\")\n"#Register event
-	result = result + "function " + "Codeic_Event_" + tostring(eventID) + "()\n"
+	result = '{}({},"{}")\n'.format(Codeic_regeisterEvent,tostring(eventID),message)#Register event
+	result += 'function Codeic_Event_{}()\n'.format(tostring(eventID))
 	for i in range(1,len(script.blocks)):
 		result = result + script.blocks[i].stringify() + "\n"
 	
@@ -38,8 +53,9 @@ def setEvent(script,message):
 	eventID = eventID+1
 	return tostring(result)
 	
-	
-build_path = ".\\build"
+def setSprite(sprite):
+	global spriteID
+	result = Codeic_regeisterSprite + "(" + spriteID + "," + sprite.name + ")\n"
 
 #Create build directory
 print "Loading scratch project file"
@@ -57,14 +73,26 @@ project = kurt.Project.load("E:\zhuom\game.sb2")
 stage = project.stage
 global_script.write(Codeic_setBackgroundColor+str(stage.COLOR)+"\n")
 for variable in project.variables:
-	global_script.write(setVariable(variable,project.variables[variable].value))
+	global_script.write(setVariable(tostring(variable),project.variables[variable].value))
 	
 for list in project.lists:
 	if not list=="":global_script.write(setList(tostring(list),project.lists[list]))	
 
+eventID = 0
 for script in stage.scripts:
 	if script.blocks[0].type.text == label_whenReceive:
 		global_script.write(setEvent(script,script.blocks[0].args[0]))
+		
+for sound in stage.sounds:
+	sound.save(build_path+"\\"+sound.name+".wav")
+
+for costume in stage.costumes:
+	costume.save(build_path+"\\"+costume.name+".png")
+
+#Sprite
+spriteID = 0
+for sprite in project.sprites:
+	print sprite.name
 
 #Close
 global_script.flush()
